e7d9afce7557987b90a6aa12859c3091ed488d7f
Joel Parsons <joel@brightfire.eu>
2013-11-04 23:38:34 +0000

    view controller for menu

---
+++ Cola Shaker/CLSMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  CLSMenuViewController.h
+//  Cola Shaker
+//
+//  Created by Joel Parsons on 04/11/2013.
+//  Copyright (c) 2013 Joel Parsons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CLSMenuViewController : UIViewController
+
+@end
---
+++ Cola Shaker/CLSMenuViewController.m
@@ -0,0 +1,38 @@
+//
+//  CLSMenuViewController.m
+//  Cola Shaker
+//
+//  Created by Joel Parsons on 04/11/2013.
+//  Copyright (c) 2013 Joel Parsons. All rights reserved.
+//
+
+#import "CLSMenuViewController.h"
+
+@interface CLSMenuViewController ()
+
+@end
+
+@implementation CLSMenuViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
