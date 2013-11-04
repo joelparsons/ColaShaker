249adfa66a7d4b35866ae0e8f86b55619b00ed2a
Joel Parsons <joel@brightfire.eu>
2013-11-04 23:38:25 +0000

    added a ascend for decorative menu

---
+++ Cola Shaker/CLSMenuScene.m
@@ -0,0 +1,13 @@
+//
+//  CLSMenuScene.m
+//  Cola Shaker
+//
+//  Created by Joel Parsons on 04/11/2013.
+//  Copyright (c) 2013 Joel Parsons. All rights reserved.
+//
+
+#import "CLSMenuScene.h"
+
+@implementation CLSMenuScene
+
+@end
