de27a14bfe7c14287056d8b67e28d2c95279dd02
Joel Parsons <joel@brightfire.eu>
2013-11-05 19:19:25 +0000

    added game progression delegate

---
+++ Cola Shaker/CLSGameProgressionDelegate.h
@@ -0,0 +1,13 @@
+//
+//  CLSGameProgressionDelegate.h
+//  Cola Shaker
+//
+//  Created by Joel Parsons on 05/11/2013.
+//  Copyright (c) 2013 Joel Parsons. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol CLSGameProgressionDelegate <NSObject>
+-(void)clsGameSceneDidFinish:(SKScene *)scene;
+@end
