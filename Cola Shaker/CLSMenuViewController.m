fa19f3086b7916a1c15cd0552dc5c779a42be8bc
Joel Parsons <joel@brightfire.eu>
2013-11-05 10:57:23 +0000

    implemented small bet functionality in game menu

--- Cola Shaker/CLSMenuViewController.m
+++ Cola Shaker/CLSMenuViewController.m
@@ -7,12 +7,35 @@
 //
 
 #import "CLSMenuViewController.h"
+#import <SmallBet/SmallBet.h>
 
 @interface CLSMenuViewController ()
+<SBControllerDelegate, UIAlertViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UILabel *shakerLabel;
+@property (nonatomic, weak) NSTimer * shakerTimer;
+
+@property (weak, nonatomic) IBOutlet SKView *backgoundScene;
+
+@property (nonatomic, strong) SBChallenge * observedChallenge;
+@property (nonatomic) BOOL userIntendedToViewChallenges;
+@property (nonatomic, strong) NSMutableArray * observers;
 
 @end
 
 @implementation CLSMenuViewController
+
+#pragma mark - properties
+
+-(NSMutableArray *)observers{
+    if (_observers) {
+        return _observers;
+    }
+    _observers = [NSMutableArray array];
+    return _observers;
+}
+
+#pragma mark - lifecycle
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -29,10 +52,204 @@
 	// Do any additional setup after loading the view.
 }
 
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    [self startShakingLabel];
+
+    id observer = [[NSNotificationCenter defaultCenter]
+                   addObserverForName:SBChallengeStatusNewNotification
+                   object:nil
+                   queue:[NSOperationQueue mainQueue]
+                   usingBlock:^(NSNotification *note) {
+                       if (self.observedChallenge) {
+                           return ;
+                       }
+                       SBChallenge * observedChallenge = note.userInfo[SBChallengeObjectKey];
+                       self.observedChallenge = observedChallenge;
+                       UIAlertView * alertView = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"New Challenge", @"alert title")
+                                                                            message:NSLocalizedString(@"You have been challenged to a game of Moneyhole!", nil)
+                                                                           delegate:self
+                                                                  cancelButtonTitle:NSLocalizedString(@"Later", @"alert button view challenge later")
+                                                                  otherButtonTitles:NSLocalizedString(@"View", @"alert button view challenge now"), nil];
+                       [alertView show];
+                   }];
+    [self.observers addObject:observer];
+
+    observer = [[NSNotificationCenter defaultCenter]
+                addObserverForName:SBChallengeStatusDidChangeNotification
+                object:nil
+                queue:[NSOperationQueue mainQueue]
+                usingBlock:^(NSNotification *note) {
+                    if (self.observedChallenge) {
+                        return ;
+                    }
+                    SBChallenge * observedChallenge = note.userInfo[SBChallengeObjectKey];
+                    self.observedChallenge = observedChallenge;
+                    UIAlertView * alertView = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Challenge Updated", @"alert title")
+                                                                         message:NSLocalizedString(@"One of your challenges has been updated!", nil)
+                                                                        delegate:self
+                                                               cancelButtonTitle:NSLocalizedString(@"Later", @"alert button view challenge later")
+                                                               otherButtonTitles:NSLocalizedString(@"View", @"alert button view challenge now"), nil];
+                    [alertView show];
+                }];
+
+    [self.observers addObject:observer];
+
+    observer = [[NSNotificationCenter defaultCenter]
+                  addObserverForName:UIApplicationDidBecomeActiveNotification
+                  object:nil
+                  queue:[NSOperationQueue mainQueue]
+                  usingBlock:^(NSNotification *note) {
+                      [self startShakingLabel];
+                  }];
+}
+
+-(void)viewDidDisappear:(BOOL)animated{
+    [super viewDidDisappear:animated];
+
+    for (id observer in self.observers) {
+        [[NSNotificationCenter defaultCenter] removeObserver:observer];
+    }
+    self.observers = nil;
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark - UIViewController
+
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
+
+}
+
+-(BOOL)prefersStatusBarHidden{
+    return YES;
+}
+
+#pragma mark - CLSMenuViewController
+
+-(void)startShakingLabel{
+    if (self.shakerTimer) {
+        [self.shakerTimer invalidate];
+        self.shakerTimer = nil;
+    }
+
+    NSTimer * timer = [NSTimer timerWithTimeInterval:0.25
+                                              target:self
+                                            selector:@selector(shakeLabel)
+                                            userInfo:nil
+                                             repeats:YES];
+    [[NSRunLoop mainRunLoop] addTimer:timer
+                              forMode:NSRunLoopCommonModes];
+    self.shakerTimer = timer;
+}
+
+-(void)shakeLabel{
+    if (CGAffineTransformIsIdentity(self.shakerLabel.transform)) {
+        self.shakerLabel.transform = CGAffineTransformMakeTranslation(10, 0);
+    }
+    else{
+        self.shakerLabel.transform = CGAffineTransformIdentity;
+    }
+}
+
+- (void)showChallengeListWithChallenge:(SBChallenge *)challenge {
+    SBChallengesViewController * challengeListViewController = [[SBChallengesViewController alloc] init];
+    challengeListViewController.delegate = self;
+
+    if (challenge) {
+        [challengeListViewController showDetailForChallenge:challenge];
+    }
+
+    [self presentViewController:challengeListViewController
+                       animated:YES
+                     completion:^{
+
+                     }];
+}
+
+#pragma mark - target Action
+
+- (IBAction)challengeButtonTapped:(id)sender {
+    SBChallengeCreateViewController * challengeCreateViewController = [[SBChallengeCreateViewController alloc] initWithDelegate:self];
+
+    [self presentViewController:challengeCreateViewController
+                       animated:YES
+                     completion:^{
+
+                     }];
+}
+
+- (IBAction)yourChallengesButtonTapped:(id)sender {
+    if ([[SBSmallbetManager defaultManager] currentPlayer] == nil) {
+        self.userIntendedToViewChallenges = YES;
+        SBLoginViewController * loginViewController = [[SBLoginViewController alloc] initWithDelegate:self];
+        [self presentViewController:loginViewController
+                           animated:YES
+                         completion:^{
+
+                         }];
+        return;
+    }
+    [self showChallengeListWithChallenge:nil];
+}
+
+- (IBAction)accountButtonTapped:(id)sender {
+    SBLoginViewController * accountViewController = [[SBLoginViewController alloc] initWithDelegate:self];
+    [self presentViewController:accountViewController
+                       animated:YES
+                     completion:^{
+
+                     }];
+}
+
+#pragma mark - SBControllerDelegate
+
+-(void)SBViewController:(UIViewController *)controller didFinishWithResult:(SBControllerResultType)result object:(id)object error:(NSError *)error{
+    //This is the delegate method for all the smallbet controllers
+    //we can detect which one we are talking about by class
+    [self dismissViewControllerAnimated:YES completion:^{
+        if ([controller isKindOfClass:[SBLoginViewController class]]) {
+            switch (result) {
+                case SBControllerResultTypeSuccess:
+                    if (self.userIntendedToViewChallenges) {
+                        [self showChallengeListWithChallenge:nil];
+                    }
+                    self.userIntendedToViewChallenges = NO;
+                    break;
+                case SBControllerResultTypeCanceled:
+                case SBControllerResultTypeFailed:
+                default:
+                    break;
+            }
+        }
+        if ([controller isKindOfClass:[SBChallengesViewController class]]) {
+            switch (result) {
+                case SBControllerResultTypeSuccess:{
+                    //when selecting challenges if the result is success then the object
+                    //is the SBChallenge the user has selected to play right now!!
+
+                }
+                    break;
+                case SBControllerResultTypeCanceled:
+                case SBControllerResultTypeFailed:
+                default:
+                    break;
+            }
+        }
+    }];
+}
+
+
+-(void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex{
+    if (buttonIndex == alertView.firstOtherButtonIndex) {
+        [self showChallengeListWithChallenge:self.observedChallenge];
+    }
+    self.observedChallenge = nil;
 }
 
 @end
